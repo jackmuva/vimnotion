d6f726fcc6a7433785296a41378f3f699b0d5af7
jackmuva <jackmu@umich.edu>
2025-08-02 08:22:59 -0700

    using themes; switch themes dynamically and set better defaults

--- src/components/custom/vim-editor.tsx
+++ src/components/custom/vim-editor.tsx
@@ -3,7 +3,7 @@ import { basicSetup, EditorView } from 'codemirror';
 import { vim } from "@replit/codemirror-vim"
 import { useEffect, useState } from 'react';
 import { Compartment } from '@codemirror/state';
-import { customTheme } from './editor-styling';
+import { bespin as darkTheme, rosePineDawn as lightTheme } from 'thememirror';
 
 export const VimEditor = () => {
 	const [vimEditor, setVimEditor] = useState<EditorView | null>(null);
@@ -22,7 +22,8 @@ export const VimEditor = () => {
 				vim(),
 				// include the default keymap and all other keymaps you want to use in insert mode
 				basicSetup,
-				themeCompartment.of(customTheme) // Apply initial theme
+				//themeCompartment.of(customTheme) // Apply initial theme
+				lightTheme,
 			],
 			parent: document.querySelector('#vim-editor')!,
 		})
@@ -31,9 +32,7 @@ export const VimEditor = () => {
 
 	return (
 		<div id='vim-editor'
-			className='w-11/12 h-11/12 bg-slate-700 rounded-sm 
-			p-1 overflow-y-scroll'>
-
+			className='w-11/12 h-11/12 overflow-y-scroll'>
 		</div>
 	)
 
