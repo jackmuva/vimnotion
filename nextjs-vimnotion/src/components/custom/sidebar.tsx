569d80a42d8f409fd038ea7921844d72f9f61f31
jackmuva <jackmu@umich.edu>
2025-08-05 22:15:36 -0700

    initiating github oauth flow

--- nextjs-vimnotion/src/components/custom/sidebar.tsx
+++ nextjs-vimnotion/src/components/custom/sidebar.tsx
@@ -5,7 +5,7 @@ export const Sidebar = () => {
 
 	useEffect(() => {
 		if (!localStorage.getItem("authenticated")) {
-			fetch(`${process.env.BACKEND_BASE_URL}/api/auth/check`, {
+			fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/auth/check`, {
 				method: "GET"
 			}).then((authReq) => {
 				authReq.json().then((authRes) => {
@@ -27,12 +27,13 @@ export const Sidebar = () => {
 
 	return (
 		<div className="h-full w-96 pt-14 p-4 bg-background-muted/10 flex flex-col items-center">
-			{!authenticated && <button id="sidebar" className="border-2 rounded-sm cursor-pointer bg-foreground-muted/50 
+			{!authenticated && <a id="sidebar" className="border-2 rounded-sm cursor-pointer bg-foreground-muted/50 
 				hover:bg-background-muted/50 flex items-center justify-center py-1 px-2 w-fit 
-				space-x-2">
+				space-x-2" href={`https://github.com/login/oauth/authorize?scope=user:email&
+						client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}>
 				<img src={"./github-logo.png"} className="h-8 w-8" />
 				<p>login to save pages</p>
-			</button>}
+			</a>}
 		</div>
 	);
 }
