e3b8334a839aee212cb89a20a9f7392712b84ec8
jackmuva <jackmu@umich.edu>
2025-08-13 18:22:19 -0700

    have turso driver

--- go-vimnotion/utils/utils.go
+++ go-vimnotion/utils/utils.go
@@ -9,13 +9,12 @@ import (
 )
 
 type EnvVars struct {
-	GithubClientId  string
-	GithubSecretKey string
-	AuthSecret      string
-	FrontendBaseUrl string
-	Neo4jUri        string
-	Neo4jUsername   string
-	Neo4jPassword   string
+	GithubClientId   string
+	GithubSecretKey  string
+	AuthSecret       string
+	FrontendBaseUrl  string
+	TursoDatabaseUrl string
+	TursoAuthToken   string
 }
 
 var envVars *EnvVars
@@ -29,13 +28,12 @@ func GetEnv() *EnvVars {
 		}
 
 		envVars = &EnvVars{
-			GithubClientId:  os.Getenv("GITHUB_CLIENT_ID"),
-			GithubSecretKey: os.Getenv("GITHUB_SECRET_KEY"),
-			AuthSecret:      os.Getenv("AUTH_SECRET"),
-			FrontendBaseUrl: os.Getenv("FRONTEND_BASE_URL"),
-			Neo4jUri:        os.Getenv("NEO4J_URI"),
-			Neo4jUsername:   os.Getenv("NEO4J_USERNAME"),
-			Neo4jPassword:   os.Getenv("NEO4J_PW"),
+			GithubClientId:   os.Getenv("GITHUB_CLIENT_ID"),
+			GithubSecretKey:  os.Getenv("GITHUB_SECRET_KEY"),
+			AuthSecret:       os.Getenv("AUTH_SECRET"),
+			FrontendBaseUrl:  os.Getenv("FRONTEND_BASE_URL"),
+			TursoDatabaseUrl: os.Getenv("TURSO_DATABASE_URL"),
+			TursoAuthToken:   os.Getenv("TURSO_AUTH_TOKEN"),
 		}
 	})
 	return envVars
