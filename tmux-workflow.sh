a305718be477b2d7717347720560e023847a652c
jackmuva <jackmu@umich.edu>
2025-08-26 23:31:04 -0700

    updated tmux script

--- tmux-workflow.sh
+++ tmux-workflow.sh
@@ -9,14 +9,17 @@ then
     tmux new-session -d -s $SESSION
 
     # Name first Pane and start zsh
-    tmux rename-window -t 0 'nvim-win'
-    tmux send-keys -t 'nvim-win' 'nvim' C-m
+    tmux rename-window -t 0 'nvim'
+    tmux send-keys -t 'nvim' 'nvim' C-m
 
     tmux new-window -t $SESSION:1 -n 'next-server'
     tmux send-keys -t 'next-server' 'cd nextjs-vimnotion' C-m 'npm run dev' C-m
 
     tmux new-window -t $SESSION:2 -n 'go-server'
     tmux send-keys -t 'go-server' 'cd go-vimnotion' C-m
+
+    tmux new-window -t $SESSION:3 -n 'opencode'
+    tmux send-keys -t 'opencode' 'opencode' C-m
 fi
 
 tmux attach-session -t $SESSION:0
