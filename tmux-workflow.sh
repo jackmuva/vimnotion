428ba64019be66bc34307da286e6c844954d4deb
Jack <jackmu@umich.edu>
2025-08-26 22:16:41 -0700

    tmux script to start new panes for front and backend

---
+++ tmux-workflow.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SESSION="VIMNOTION-SESSION"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+
+    # Name first Pane and start zsh
+    tmux rename-window -t 0 'nvim-win'
+    tmux send-keys -t 'nvim-win' 'nvim' C-m
+
+    tmux new-window -t $SESSION:1 -n 'next-server'
+    tmux send-keys -t 'next-server' 'cd nextjs-vimnotion' C-m 'npm run dev' C-m
+
+    tmux new-window -t $SESSION:2 -n 'go-server'
+    tmux send-keys -t 'go-server' 'cd go-vimnotion' C-m
+fi
+
+tmux attach-session -t $SESSION:0
