14fac41e65cb635b0d4bd9506ecfe021132f8674
Jack <jackmu@umich.edu>
2025-09-21 13:37:51 -0700

    added sqlite setup and queries

--- go-vimnotion/turso/turso.go
+++ go-vimnotion/turso/turso.go
@@ -25,10 +25,20 @@ func ConnectTurso() *sql.DB {
 
 	db.SetConnMaxIdleTime(9 * time.Second)
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS test (email TEXT PRIMARY KEY, name TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS Users (email TEXT PRIMARY KEY, name TEXT)")
 	if err != nil {
 		fmt.Printf("error creating table: %s", err)
 	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS DirectoryStructure(email TEXT PRIMARY KEY, structure TEXT)")
+	if err != nil {
+		fmt.Printf("error creating table: %s", err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS VnObjects(id TEXT PRIMARY KEY, name TEXT, IsFile BOOLEAN)")
+	if err != nil {
+		fmt.Printf("error creating table: %s", err)
+	}
+
 	return db
 }
 
@@ -37,8 +47,20 @@ type User struct {
 	Name  string
 }
 
-func QueryUsers(db *sql.DB) {
-	rows, err := db.Query("SELECT * FROM users")
+type DirectoryStructure struct {
+	Email     string
+	Structure string
+}
+
+type VnObject struct {
+	Id       string
+	Name     string
+	IsFile   bool
+	Contents string
+}
+
+func GetUser(db *sql.DB, email string) []User {
+	rows, err := db.Query("SELECT * FROM Users WHERE email='?'", email)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
 		os.Exit(1)
@@ -52,7 +74,6 @@ func QueryUsers(db *sql.DB) {
 
 		if err := rows.Scan(&user.Email, &user.Name); err != nil {
 			fmt.Println("Error scanning row:", err)
-			return
 		}
 
 		users = append(users, user)
@@ -61,5 +82,95 @@ func QueryUsers(db *sql.DB) {
 
 	if err := rows.Err(); err != nil {
 		fmt.Println("Error during rows iteration:", err)
+	}
+	return users
+}
+
+func GetDirectoryStructure(db *sql.DB, email string) []DirectoryStructure {
+	rows, err := db.Query("SELECT * FROM DirectoryStructure WHERE email='?'", email)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var directoryStructures []DirectoryStructure
+
+	for rows.Next() {
+		var directoryStructure DirectoryStructure
+
+		if err := rows.Scan(&directoryStructure.Email, &directoryStructure.Structure); err != nil {
+			fmt.Println("Error scanning row:", err)
+		}
+
+		directoryStructures = append(directoryStructures, directoryStructure)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+	return directoryStructures
+}
+
+func GetVnObjectById(db *sql.DB, id string) []VnObject {
+	rows, err := db.Query("SELECT * FROM VnObjects WHERE id='?'", id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+	}
+	defer rows.Close()
+
+	var vnObjects []VnObject
+
+	for rows.Next() {
+		var vnObject VnObject
+
+		if err := rows.Scan(&vnObject.Id, &vnObject.Name, &vnObject.IsFile, &vnObject.Contents); err != nil {
+			fmt.Println("Error scanning row:", err)
+		}
+
+		vnObjects = append(vnObjects, vnObject)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+	return vnObjects
+}
+
+func InsertUser(db *sql.DB, user User) {
+	_, err := db.Exec("INSERT INTO User (email, name) VALUES(?, ?)", user.Email, user.Name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute insert: %v\n", err)
+	}
+}
+
+func InsertDirectoryStructure(db *sql.DB, dirStruct DirectoryStructure) {
+	_, err := db.Exec("INSERT INTO DirectoryStructure (email, structure) VALUES(?, ?)", dirStruct.Email, dirStruct.Structure)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute insert: %v\n", err)
+	}
+}
+
+func InsertVnObject(db *sql.DB, vnObject VnObject) {
+	_, err := db.Exec("INSERT INTO VnObject (id, name, isFile, Contents ) VALUES(?, ?)",
+		vnObject.Id, vnObject.Name, vnObject.IsFile, vnObject.Contents)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute insert: %v\n", err)
+	}
+}
+
+func UpdateDirectoryStructure(db *sql.DB, dirStruct DirectoryStructure) {
+	_, err := db.Exec("UPDATE DirectoryStructure SET structure=? WHERE email=?",
+		dirStruct.Structure, dirStruct.Email)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute update: %v\n", err)
+	}
+}
+
+func UpdateVnObject(db *sql.DB, vnObject VnObject) {
+	_, err := db.Exec("UPDATE VnObject SET Name=?, Contents=? WHERE id=?",
+		vnObject.Name, vnObject.Contents, vnObject.Id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute update: %v\n", err)
 	}
 }
