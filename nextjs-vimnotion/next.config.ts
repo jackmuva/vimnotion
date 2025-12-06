c3fc788d0adf55fa8b636c65d7ba9a55af266f0b
Jack <jackmu@umich.edu>
2025-12-06 10:20:07 -0800

    removed rewrites

--- nextjs-vimnotion/next.config.ts
+++ nextjs-vimnotion/next.config.ts
@@ -2,24 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	devIndicators: false,
-	async rewrites() {
-		return {
-			beforeFiles: [
-				{
-					source: "/api/:path*",
-					destination: `${process.env.BACKEND_BASE_URL}/api/:path*`,
-				},
-				{
-					source: "/oauth/:path*",
-					destination: `${process.env.BACKEND_BASE_URL}/oauth/:path*`,
-				},
-				{
-					source: "/image/:path*",
-					destination: `${process.env.BACKEND_BASE_URL}/image/:path*`,
-				},
-			],
-		};
-	},
 };
 
 export default nextConfig;
